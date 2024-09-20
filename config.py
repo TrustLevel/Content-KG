65d44759b1c65e49346a7db0c4a9bf137bca41da
Dominik Tilman <80850851+dominiktilman@users.noreply.github.com>
2024-09-20 09:11:45 +0000

    modified:   config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # SingularityNET endpoint
-SNET_ENDPOINT = "your_snet_service_endpoint"
+SNET_ENDPOINT = "https://beta.singularitynet.io/services/trustlevel-bias-service/grpc"
 
 # Add your wallet or Metamask-related details for authentication
-METAMASK_ADDRESS = "your_metamask_address"
+METAMASK_ADDRESS = "0x1099cdc225cB70C97E694f1A881526ee23995124"
 PRIVATE_KEY = "your_private_key"
