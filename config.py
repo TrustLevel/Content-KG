498ecbb3ca2900cbce4ce1ef426b2509882f01cb
Dominik Tilman <80850851+dominiktilman@users.noreply.github.com>
2024-09-20 09:15:59 +0000

    modified:   config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ SNET_ENDPOINT = "https://beta.singularitynet.io/services/trustlevel-bias-service
 
 # Add your wallet or Metamask-related details for authentication
 METAMASK_ADDRESS = "0x1099cdc225cB70C97E694f1A881526ee23995124"
-PRIVATE_KEY = "your_private_key"
+PRIVATE_KEY = "eeb2ddaccc79a803835f8cf5adf225683c349b4bb9f1c47804b37807ce6be842"
