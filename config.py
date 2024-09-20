3ee761334e3160609c25f122af244045583d6586
Dominik Tilman <80850851+dominiktilman@users.noreply.github.com>
2024-09-20 09:27:47 +0000

    deleted:    config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# SingularityNET endpoint
-SNET_ENDPOINT = "https://beta.singularitynet.io/services/trustlevel-bias-service/grpc"
-
-# Add your wallet or Metamask-related details for authentication
-METAMASK_ADDRESS = "0x1099cdc225cB70C97E694f1A881526ee23995124"
-PRIVATE_KEY = "eeb2ddaccc79a803835f8cf5adf225683c349b4bb9f1c47804b37807ce6be842"
